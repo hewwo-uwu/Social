5affff208097a93f303f9cdee0b0ef743726cbc5
Devon <Dvowen@cox.net>
2019-07-14 18:22:12 -0700

    dummy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+
+  render() {
+    const loginStyle = {
+      backgroundColor: '#2a2a2a',
+      textAlign: 'center',
+      color: '#fff',
+      display:'flex',
+      justifyContent: 'center',
+    }
+    const forgot = {
+      marginTop:'50px',
+    }
+
+    return (
+      <div style={loginStyle}>
+        <div className="login">
+        <div id="logo">
+        <h1 id="loginLogo">Social</h1>
+        </div>
+          <div id="loginInputs" className="flex flex-column">
+            <input className="logins" type="text" placeholder="Username Daddy UwU" />
+            <input className="logins" type="password" placeholder="Password Daddy UwU" />
+          </div>
+          <div id="bottomLogin"className="flex flex-column">
+            <div id="forgotCreate" className="flex flex-row justify-content-around" style={forgot}>
+              <h3>hewwo</h3>
+              <h3>hewwo</h3>
+            </div>
+          <Button className="hewwo" variant="contained" color="primary">
+                Login
+          </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
